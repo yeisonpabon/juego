a0abc702068871bcad852b9fd336db2271287bc7
maoyinggomez <mao.gomez@correounivalle.edu.com>
2025-06-19 01:22:06 -0500

    s

---
+++ model/db.py
@@ -0,0 +1,12 @@
+# db.py
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        user="root",
+        password="",  # tu contraseña si tienes
+        host="localhost",
+        database="videojuego"
+    )
+
+
