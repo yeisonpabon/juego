d3cc04daac12fe9124bad4321e057dfe451fafed
maoyinggomez <mao.gomez@correounivalle.edu.com>
2025-06-19 01:51:55 -0500

    qwerty

--- ejecutarloggin.py
+++
@@ -1,9 +0,0 @@
-from loggin import Loggin
-from ventana2 import SessionWindow
-
-if __name__ == "__main__":
-    import customtkinter as ctk
-    root = ctk.CTk()
-    root.withdraw()
-    SessionWindow(root)
-    root.mainloop()
